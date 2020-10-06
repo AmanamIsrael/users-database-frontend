c919b1572685b10daaa53e921814b260b4b90f0b
Amanam Israel <amanam42@hotmail.com>
2020-10-06 20:15:51 +0100

    frontend ready

--- src/components/partials/content.vue
+++ src/components/partials/content.vue
@@ -1,26 +1,78 @@
 <template>
     <div>
-        <button data-toggle="modal" data-target="formModal" class="btn btn-primary d-block ml-auto my-3"><i class="fas fa-plus"></i> Add new user</button>
-         <!-- modal -->
-         <div class="modal fade" id="formModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="formModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add new User</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save User</button>
-                </div>
-                </div>
-            </div>
-            </div>
+        <div class="wrapper">
+            <b-button v-b-modal.form-modal variant="primary d-block ml-auto my-3"><i class="fas fa-plus"></i> Add new user</b-button>
+        <b-modal id="form-modal" hide-footer  title="Add User">
+            <b-form @submit="addUser" @reset="resetModal" noValidate="true">
+                <b-form-group
+                        id="first"
+                        label="First Name"
+                        label-for="firstname"
+                        >
+                        <b-form-input
+                        id="firstname"
+                        v-model="userDetails.first_name"
+                        type="text"
+                        required
+                        placeholder="Enter First name"
+                        ></b-form-input>
+                </b-form-group>
+                <b-form-group
+                        id="last"
+                        label="First Name"
+                        label-for="lastname"
+                        >
+                        <b-form-input
+                        id="lastname"
+                        v-model="userDetails.last_name"
+                        type="text"
+                        required
+                        placeholder="Enter Last name"
+                        ></b-form-input>
+                </b-form-group>
+                <b-form-group
+                        id="age"
+                        label="First Name"
+                        label-for="userage"
+                        >
+                        <b-form-input
+                        id="userage"
+                        v-model="userDetails.age"
+                        type="text"
+                        required
+                        placeholder="Enter Age"
+                        ></b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="success mr-3">Add User</b-button>
+                <b-button type="reset" variant="secondary">Reset</b-button>
+            </b-form>
+        </b-modal>
+
+        <div class="mt-5">
+             <b-table-simple hover responsive>
+                 <b-thead>
+                     <b-th>First Name</b-th>
+                     <b-th>Last Name</b-th>
+                     <b-th>Age</b-th>
+                     <b-th>Actions</b-th>
+                 </b-thead>
+                 <b-tbody>
+                     <b-td>man</b-td>
+                     <b-td>man</b-td>
+                     <b-td>man</b-td>
+                     <b-td>
+                         <div>
+                             <b-dropdown variant="light" id="dropdown-1" text="Action">
+                                 <b-dropdown-item>Edit</b-dropdown-item>
+                                 <b-dropdown-item>Delete</b-dropdown-item>
+                             </b-dropdown>
+                        </div>
+                    </b-td>
+                 </b-tbody>
+
+            </b-table-simple>
+        </div>
+        </div>
     </div>
 </template>
 <script>
@@ -43,9 +95,7 @@ export default {
     }, 
     methods: {
         addUser(){
-            if(!this.isFormValid()){
-                return
-            }
+            event.preventDefault();
             console.log(this.userDetails)
             this.$nextTick(()=> {
                 this.$bvModal.hide('formModal')
@@ -55,7 +105,7 @@ export default {
         this.first_name = ''
         this.last_name = ''
         this.age = ''
-        },
+        }
     }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "./router";
+import bootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 
 Vue.use(VueRouter);
+Vue.use(bootstrapVue)
 
 const router = new VueRouter({
     routes,
